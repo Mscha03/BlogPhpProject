a2fabc26fe4d01277a28677f4aa0c8bdad0e8c70
MohammadSaleh Sedighi <142793212+Mscha03@users.noreply.github.com>
2025-12-02 02:03:36 +0330

    کلاس Request برای مدیریت درخواست‌های HTTP اضافه شد
    یک کلاس Request معرفی می‌کند تا داده‌های درخواست HTTP شامل متد، آدرس و ویژگی‌های GET، POST و FILES را دربرگیرد.

---
+++ src/Classes/Request.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Classes;
+
+class Request
+{
+    private array $attributes = [];
+    private string $method;
+    private string $url;
+
+    /**
+     * @param array $attributes
+     * @param $url
+     */
+    public function __construct(array $attributes, $url)
+    {
+        $this->attributes = $attributes;
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+        if($this->method === 'POST'){
+            foreach ($_POST as $key => $value) {
+                $this->attributes[$key] = $value;
+            }
+
+            foreach ($_FILES as $key => $value) {
+                $this->attributes[$key] = $value;
+            }
+        } else {
+            foreach ($_GET as $key => $value) {
+                $this->attributes[$key] = $value;
+            }
+        }
+    }
+
+    public function _get(string $name)
+    {
+        if(array_key_exists($name, $this->attributes)){
+            return $this->attributes[$name];
+        }
+        return null;
+    }
+
+    public function has(string $name): bool
+    {
+        if(isset($this->attributes[$name])){
+            return true;
+        }
+        return false;
+    }
+
+    public function get(string $name)
+    {
+        if(array_key_exists($name, $this->attributes)){
+            return $this->attributes[$name];
+        }
+        return null;
+    }
+
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+}
