a6091c19ec645e1b75b7c8600b2ac5466a1e5b28
MohammadSaleh Sedighi <142793212+Mscha03@users.noreply.github.com>
2025-12-05 10:45:17 +0330

    + اضافه کردن صفحه ErrorPage.php + اضافه کردن صفحه NotFoundPage.php

---
+++ src/Templates/NotFoundPage.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Templates;
+
+use App\Exceptions\DoesNotExistsException;
+use App\Templates\Template;
+
+class NotFoundPage extends Template
+{
+    private string $message;
+
+    /**
+     * @param $message
+     * @throws DoesNotExistsException
+     */
+    public function __construct($message)
+    {
+        parent::__construct();
+        $this->message = $message;
+        $this->title = $message;
+    }
+
+
+    public function renderPage(): void
+    {
+        ?>
+        <!doctype html>
+        <html lang="en">
+            <?php $this->getHead() ?>
+            <body>
+                <main>
+                    <section id="content">
+                        <?= $this->message ?>
+                        <br/>
+                        <a href="index.php">go home</a>
+                    </section>
+                </main>
+            </body>
+        </html>
+        <?php
+    }
+}
