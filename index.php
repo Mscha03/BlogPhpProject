a6378dbad4daf5b037ec2137158ed8fe205d5dd7
MohammadSaleh Sedighi <142793212+Mscha03@users.noreply.github.com>
2025-12-05 10:46:14 +0330

    + ساخت صفحه Index.php

--- index.php
+++ index.php
@@ -2,10 +2,39 @@
 
 require 'vendor/autoload.php';
 
+use App\Classes\Request;
+use App\Exceptions\DoesNotExistsException;
+use App\Exceptions\NotFoundException;
 use App\Templates\CategoryPage;
+use App\Templates\ErrorPage;
 use App\Templates\MainPage;
+use App\Templates\NotFoundPage;
+use App\Templates\SearchPage;
 use App\Templates\SinglePage;
 
-
-$page = new CategoryPage();
-$page->renderPage();
+$page = null;
+try {
+    $request = new Request();
+    switch ($request->get('action')) {
+        case 'single':
+            $page = new SinglePage();
+            break;
+        case 'search':
+            $page = new SearchPage();
+            break;
+        case 'category':
+            $page = new CategoryPage();
+            break;
+        case null:
+            $page = new MainPage();
+            break;
+        default:
+            throw new NotFoundException();
+    }
+} catch (DoesNotExistsException | NotFoundException $e) {
+    $page = new NotFoundPage($e->getMessage());
+} catch (Exception $e) {
+    $page = new ErrorPage($e->getMessage());
+} finally {
+    $page->renderPage();
+}
