202f5183ea8a300b4bc40bd6c5eea676e72a1a4f
MohammadSaleh Sedighi <142793212+Mscha03@users.noreply.github.com>
2025-12-05 10:47:07 +0330

    ارتقای پروژه از php 8.2 به 8.4

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'mscha/blogphpproject',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ea99ffd67140819387e8866192610e0c92984bca',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'mscha/blogphpproject' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ea99ffd67140819387e8866192610e0c92984bca',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
